707df174131f96c8493425bf42acddb79bce07df
Euclidean <guoanjie@gmail.com>
2020-12-01 15:26:25 -0600

    finished fig 1.2

---
+++ notebooks/datasets/zip.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from pathlib import Path
+
+
+X_train = [np.loadtxt(
+    Path(__file__).absolute().parents[2] / "data" / "zip.digits" / f"train.{d}",
+    delimiter=',',
+).reshape(-1, 16, 16) for d in range(10)]
