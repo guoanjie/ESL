57b08610bac26e353fd1eff3dc7c3b5e00bae2f0
Euclidean <guoanjie@gmail.com>
2020-12-02 15:54:15 -0600

    finished fig 3.5

--- notebooks/models.py
+++ notebooks/models.py
@@ -13,15 +13,22 @@ class BestSubsetLinearRegression(LinearRegression):
 
     def fit(self, X, y, sample_weight=None):
         X, y = self._validate_data(X, y)
+        self.all_mse_ = []
         best_combination, best_mse = None, np.inf
         best_intercep_, best_coef_ = None, None
         for combination in combinations(range(X.shape[1]), self.k):
-            super().fit(X[:, combination], y)
-            mse = mean_squared_error(y, self.predict(X[:, combination]))
+            if combination:
+                super().fit(X[:, combination], y, sample_weight)
+                mse = mean_squared_error(y, self.predict(X[:, combination]))
+            else:
+                self.intercept_ = y.mean()
+                self.coef_ = []
+                mse = y.var()
+            self.all_mse_.append(mse)
             if mse < best_mse:
                 best_combination, best_mse = combination, mse
                 best_intercept_, best_coef_ = self.intercept_, self.coef_
-        super().fit(X, y)
+        super().fit(X, y, sample_weight)
         self.intercept_ = best_intercept_
         self.coef_[:] = 0
         self.coef_[list(best_combination)] = best_coef_
