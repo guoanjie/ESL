a903df380b29b49459cb5c1ef7a9ba6f8fea44e2
Euclidean <guoanjie@gmail.com>
2021-01-16 19:31:19 -0600

    finished fig 4.4

--- notebooks/datasets/vowel.py
+++ notebooks/datasets/vowel.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 
+from numpy import linalg as LA
 from pathlib import Path
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 data_dir = Path(__file__).absolute().parents[2] / "data"
@@ -12,3 +15,29 @@ X_test = df_test.filter(regex='^x\.')
 y_test = df_test['y']
 
 del data_dir
+
+def plot_lda(*coords, ax=None):
+    clf = LinearDiscriminantAnalysis(store_covariance=True)
+    clf.fit(X_train, y_train)
+    w, v = LA.eig(clf.covariance_)
+    W_rsqrt = v @ np.diag(np.power(w, -.5)) @ v.T
+    M_ = clf.means_ @ W_rsqrt
+    B_ = np.cov(M_.T)
+    D, V_ = LA.eig(B_)
+    V = - W_rsqrt @ V_
+    palette = [
+        "#000000", "#0718F5", "#963330", "#9133E7", "#ED9135", "#7DFBFD",
+        "#74808E", "#FBEC98", "#000000", "#E73323", "#7DFA4C",
+    ]
+    ax = (X_train @ V).plot.scatter(
+        coords[0] - 1, coords[1] - 1, ax=ax, alpha=.25,
+        c=y_train.map(lambda y: palette[y - 1]),
+    )
+    ax = pd.DataFrame(data=clf.means_ @ V).plot.scatter(
+        coords[0] - 1, coords[1] - 1, ax=ax,
+        c=palette,
+    )
+    ax.set_xlabel(f"Coordinate {coords[0]}")
+    ax.set_ylabel(f"Coordinate {coords[1]}")
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
