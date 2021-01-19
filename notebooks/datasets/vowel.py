1ebbddb422423b660a5eeb7527ffd2c0f0558890
Euclidean <guoanjie@gmail.com>
2021-01-19 13:43:55 -0600

    use LDA transform for projection plots

--- notebooks/datasets/vowel.py
+++ notebooks/datasets/vowel.py
@@ -1,7 +1,5 @@
-import numpy as np
 import pandas as pd
 
-from numpy import linalg as LA
 from pathlib import Path
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
@@ -16,31 +14,20 @@ y_test = df_test['y']
 
 del data_dir
 
-clf = LinearDiscriminantAnalysis(store_covariance=True)
-clf.fit(X_train, y_train)
-w, v = LA.eig(clf.covariance_)
-W_rsqrt = v @ np.diag(np.power(w, -.5)) @ v.T
-M_ = clf.means_ @ W_rsqrt
-B_ = np.cov(M_.T)
-D, V_ = LA.eig(B_)
-V = - W_rsqrt @ V_
-
-del w, v, W_rsqrt, M_, B_, D, V_
 
 def plot_lda(*coords, ax=None):
     palette = [
         "#000000", "#0718F5", "#963330", "#9133E7", "#ED9135", "#7DFBFD",
         "#74808E", "#FBEC98", "#000000", "#E73323", "#7DFA4C",
     ]
-    ax = (X_train @ V).plot.scatter(
+    clf = LinearDiscriminantAnalysis().fit(X_train, y_train)
+    ax = pd.DataFrame(data=clf.transform(X_train)).plot.scatter(
         coords[0] - 1, coords[1] - 1, ax=ax, c='none', edgecolors=palette,
     )
-    ax = pd.DataFrame(data=clf.means_ @ V).plot.scatter(
+    ax = pd.DataFrame(data=clf.transform(clf.means_)).plot.scatter(
         coords[0] - 1, coords[1] - 1, ax=ax, c='none', edgecolors=palette,
         linewidth=5,
     )
     ax.set_xlabel(f"Coordinate {coords[0]}")
     ax.set_ylabel(f"Coordinate {coords[1]}")
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
     ax.set_title("Linear Discriminant Analysis")
